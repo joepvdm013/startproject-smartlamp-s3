890fcf47dd6300230aa804072f2230f5f30c3909
joepvdm013 <joepvandermeer1@gmail.com>
2021-01-22 20:32:23 +0100

    Extra commentaar toegevoegd

--- BlynkNeopixelGeenGegevens/BlynkNeopixelGeenGegevens.ino
+++ BlynkNeopixelGeenGegevens/BlynkNeopixelGeenGegevens.ino
@@ -1,28 +1,37 @@
+//SmartLamp NeoPixel
+//M.b.v. de Blynk app en de NeoPixel RGB Led Ring heb ik een lamp gemaakt die je met je telefoon kunt bedienen.
+//ingeladen libraries
 #include <Adafruit_NeoPixel.h>
 #include <SPI.h>
 #include <BlynkSimpleEsp8266.h>
 #include <ESP8266WiFi.h>
 
+
 #define PIN D1
-#define NUMPIXELS 12
+#define NUMPIXELS 12 //aantal neopixels op de ring
 #define BLYNK_PRINT Serial
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
+//setup wordt eenmalig uitgevoerd
 void setup()
 {
-Serial.begin(9600);
-Blynk.begin("key", "ssid", "wachtwoord");
+Serial.begin(9600); //serial monitor poort wordt gestart
+Blynk.begin("key", "ssid", "wachtwoord"); //Blynk wordt gestart. Blynk is een externe app voor je telefoon waarmee je een arduino (die beschikt over WiFi) kunt bedienen. Hij gebruikt hierbij de API key vanuit Blynk, en het SSID en Wachtwoord van je netwerk.
 pixels.begin();
 }
 BLYNK_WRITE(V1)
 {
 
-int R = param[0].asInt();
-int G = param[1].asInt();
-int B = param[2].asInt();
+int R = param[0].asInt(); //waarde van rood wordt omgezet naar een integer
+int G = param[1].asInt(); //waarde van green wordt omgezet naar een integer
+int B = param[2].asInt(); //waarde van blauw wordt omgezet naar een integer
+
+//RGB waardes worden geprint naar de serial monitor
 Serial.println(R);
 Serial.println(G);
 Serial.println(B);
+
+// foor loop die het aantal pixels afgaat en de juiste kleuren weergeeft op de neopixel
 for(int i=0;i<NUMPIXELS;i++){
 
 pixels.setPixelColor(i, pixels.Color(R,G,B));
@@ -30,7 +39,7 @@ pixels.setPixelColor(i, pixels.Color(R,G,B));
 pixels.show();
 }
 }
-
+//zorgt er voor dat blynk draait zodat je met de blynk app de kleuren van de app kunt veranderen
 void loop()
 {
 Blynk.run();
--- RGB_LED/RGB_LED.ino
+++ RGB_LED/RGB_LED.ino
@@ -1,8 +1,11 @@
+//Eerste POC met een RGB led met arduino
 
+//define van poorten
 #define BLUE 3
 #define GREEN 5
 #define RED 6
 
+//setup wordt eenmaal uitgevoerd
 void setup()
 {
 pinMode(RED, OUTPUT);
@@ -13,23 +16,24 @@ digitalWrite(GREEN, LOW);
 digitalWrite(BLUE, LOW);
 }
 
-
+//Variablen aangemaakt voor de values van de RGB
 int redValue;
 int greenValue;
 int blueValue;
 
-// main loop
+// loopt wordt continu afgespeeld
 void loop()
 {
-#define delayTime 10 
+#define delayTime 10 //tijd tussen het overgaan van values
 
+//rode value wordt volledig aangezet
 redValue = 255; 
 greenValue = 0;
 blueValue = 0;
 
 
 
-for(int i = 0; i < 255; i += 1) // fades out red to green full when i=255
+for(int i = 0; i < 255; i += 1) // for loop waarbij de rode value naar 0 gaat (leeg) en de groene value naar 255(vol). Hierdoor creeër je een soort overloop van rood naar groen
 {
 redValue -= 1;
 greenValue += 1;
@@ -38,11 +42,12 @@ analogWrite(GREEN, greenValue);
 delay(delayTime);
 }
 
+//groene value wordt volledig aangezet
 redValue = 0;
 greenValue = 255;
 blueValue = 0;
 
-for(int i = 0; i < 255; i += 1) // fades out green to blue full when i=255
+for(int i = 0; i < 255; i += 1) // for loop waarbij de groene value naar 0 gaat (leeg) en de blauwe value naar 255(vol). Hierdoor creeër je een soort overloop van groen naar blauw
 {
 greenValue -= 1;
 blueValue += 1;
@@ -51,11 +56,12 @@ analogWrite(BLUE, blueValue);
 delay(delayTime);
 }
 
+//blauwe value wordt volledig aangezet
 redValue = 0;
 greenValue = 0;
 blueValue = 255;
 
-for(int i = 0; i < 255; i += 1) // fades out blue to red full when i=255
+for(int i = 0; i < 255; i += 1) // for loop waarbij de blauwe value naar 0 gaat (leeg) en de rode value naar 255(vol). Hierdoor creeër je een soort overloop van blauw naar red.
 {
 
 blueValue -= 1;
